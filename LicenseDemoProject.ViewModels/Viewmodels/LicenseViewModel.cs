13833eafc2f680acf1c42e47eabb252d59db64ea
Sebastian <sebastian@verion.ind.br>
2024-11-10 04:53:38 -0300

    Sebas: [add] Checkpoint

--- LicenseDemoProject.ViewModels/Viewmodels/LicenseViewModel.cs
+++ LicenseDemoProject.ViewModels/Viewmodels/LicenseViewModel.cs
@@ -1,6 +1,13 @@
-﻿namespace LicenseDemoProject.ViewModels.Viewmodels;
+﻿using LicenseDemoProject.Services;
+
+namespace LicenseDemoProject.ViewModels.Viewmodels;
 
 public class LicenseViewModel : ViewModelBase
 {
-    
+    public LicenseViewModel(LicenseValidator licenseValidator)
+    {
+        LicenseValidator = licenseValidator;
+    }
+
+    public LicenseValidator LicenseValidator { get; }
 }
--- LicenseDemoProject.Views/Views/LicenseView.axaml.cs
+++ LicenseDemoProject.Views/Views/LicenseView.axaml.cs
@@ -5,13 +5,25 @@ using LicenseDemoProject.ViewModels.Container;
 using LicenseDemoProject.ViewModels.Viewmodels;
 using Avalonia.Media.Imaging;
 using System.IO;
+using Avalonia;
+using Avalonia.Interactivity;
 using LicenseDemoProjectViews.Assets;
+using QlmLicenseLib;
 
 namespace LicenseDemoProjectViews.Views;
 
 public partial class LicenseView : UserControl
 {
     private LicenseViewModel? _viewModel;
+    private string activationKey = string.Empty;
+    private string computerKey = string.Empty;
+    private string webServiceUrl = string.Empty;
+    private string computerId = "01";
+
+    private string qlmVersion = "5.0.00";
+    private string qlmUserData = string.Empty;
+
+    bool activationResult = false;
 
     public LicenseView()
     {
@@ -22,6 +34,18 @@ public partial class LicenseView : UserControl
         InitializeComponent();
 
         MyImage.Source = ByteArrayToBitmap(LicenseResources.KeyIconImg);
+    }
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+ 
+        bool needsActivation = false;
+        string errorMsg = string.Empty;
+        if (_viewModel?.LicenseValidator.ValidateLicenseAtStartup(Environment.MachineName, ref needsActivation, ref errorMsg) == false)
+        {
+            ShowViewAction?.Invoke(ViewsEnum.LicenseActivatedView);
+        }
     }
 
     public event Action<ViewsEnum>? ShowViewAction;
@@ -39,7 +63,28 @@ public partial class LicenseView : UserControl
 
     private void UnlockButton_OnTapped(object? sender, TappedEventArgs e)
     {
-        // If license unlocked
-        ShowViewAction?.Invoke(ViewsEnum.LicenseActivatedView);
+        if (_viewModel == null)
+            return;
+
+        _viewModel.LicenseValidator.QlmLicenseObject.ActivateLicense(
+            webServiceUrl,
+            LicenseKeyTextBox.Text,
+            computerId,
+            Environment.MachineName,
+            qlmVersion,
+            qlmUserData,
+            out var response);
+
+        ILicenseInfo licenseInfo = new LicenseInfo();
+        string message = string.Empty;
+        if (_viewModel.LicenseValidator.QlmLicenseObject.ParseResults(response, ref licenseInfo, ref message))
+        {
+            ShowViewAction?.Invoke(ViewsEnum.LicenseActivatedView);
+        }
+        else
+        {
+            string text = "Activation failed. " + message;
+            // Show popup with text
+        }
     }
 }
